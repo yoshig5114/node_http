108aec2a3621b3f8d5f8dee41ac6d50bc694218c
Joshua Gregory <yoshig5114@gmail.com>
2023-06-22 08:47:40 -0400

    exercise done

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
 const http = require('http');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Hello World!");
-    res.end();
+const port = 3000;
+
+http.createServer((req, res) =>{
+    let chunks = [];  
+    let { url } = req;
+    // this is a readable stream  
+    req.on("data",(chunk)=>{
+        chunks.push(chunk);
+    });
+    req.on("end",()=>{
+        const body = {
+            url: req.url,
+            method: req.method
+        };
+        const wildcard = {
+            wild: "card",
+            wacky: true
+        }
+        const about ={
+            name: "joshua",
+            stressed: true
+        }
+
+        //wild card route
+        if(url === "/"){
+            res.write(JSON.stringify(wildcard));
+        }
+        else if(url === "/about"){
+            res.write(JSON.stringify(about));
+        }
+        else if(url === "/echo"){
+            res.write(JSON.stringify(body));
+        }
+        res.end();
+        
+    });
+})
+.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`);
 });
+
 
